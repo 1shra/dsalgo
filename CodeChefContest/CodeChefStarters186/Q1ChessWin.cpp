f7b4a19a8af4a227c0977988dc99a224192efe0d
Ashraf <ashrafknight54@gmail.com>
2025-05-15 10:35:03 +0530

    14/5/2025

---
+++ CodeChefContest/CodeChefStarters186/Q1ChessWin.cpp
@@ -0,0 +1,26 @@
+/*
+link: https://www.codechef.com/problems/CHESSWIN?tab=statement
+
+Input A:
+0 1
+
+Input B:
+2 4
+*/
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a,b;
+    cin>>a>>b;
+    int c = ((b-a)+1);
+    cout<<c;
+    return 0;
+}
